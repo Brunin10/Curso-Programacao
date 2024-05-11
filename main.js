89686d3322d7a02b43fc16b0d0f4bb7dde032229
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-05-11 09:01:28 -0300

    mghdgmgm

--- main.js
+++ main.js
@@ -115,5 +115,5 @@ numeroPiso = Math.floor(4.89);
 
 console.log(`Número piso: ${numeroPiso}`);
 
-const dataInicial = new Date(1000);
+const dataInicial = new Date();
 console.log(dataInicial.toUTCString());
